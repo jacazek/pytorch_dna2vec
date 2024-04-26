77bcd5b0bb01466aa1a723b694ab6f3a7440f8b6
jake <dukob86@yahoo.com>
2024-04-25 23:34:34 -0400

    feat: switch to toml configuration

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-lib_folder = os.path.dirname(os.path.realpath(__file__))
-requirement_path = f"{lib_folder}/requirements.txt"
-install_requires = []
-if os.path.isfile(requirement_path):
-    with open(requirement_path) as f:
-        install_requires = f.read().splitlines()
-
-setup(
-    name='pytorch_dna2vec',
-    version='0.1.0',
-    packages=find_packages(),
-    install_requires=install_requires
-)
+setup()
